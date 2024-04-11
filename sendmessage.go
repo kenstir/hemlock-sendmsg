ee25369041d73ee3596a890f52ce223bb42ec7fa
kenstir <kenstir@gmail.com>
2024-04-11 11:41:57 -0400

    Add equivalent golang code to send a message

---
+++ sendmessage.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/messaging"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	opts := []option.ClientOption{option.WithCredentialsFile("service-account.json")}
+	config := &firebase.Config{ProjectID: "test-fdfb4"}
+
+	app, err := firebase.NewApp(context.Background(), config, opts...)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+
+	fcmClient, err := app.Messaging(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to initialize Messaging: %s", err) 
+	}
+
+	_, err = fcmClient.Send(context.Background(), &messaging.Message{
+		Notification: &messaging.Notification{
+			Title:    "A nice notification title",
+			Body:     "A nice notification body",
+		},
+		Token: "f2uwnT37RJqljk8b9PV4zJ:APA91bFgG03Ty5cl7epCXh4vAJg68M-yz1Uuh4YIjSv2oSy38a-XzcekEsH-SmGd_r94b2EFc8Kxo6CEYDdPRqqmqa_ykdHpxv7xjZR8QMvFwBkvV3YJC9BSOraH1hDaeUgK8wHhjhq5",
+	})
+	if err != nil {
+		log.Fatalf("Failed to send notification: %s", err) 
+	}
+}
