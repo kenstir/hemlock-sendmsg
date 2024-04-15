8c82460192d933abd6506c9829211b5895544098
kenstir <kenstir@gmail.com>
2024-04-15 13:20:43 -0400

    Use default creds file relative to exe

--- config.go
+++ config.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"os"
+	"path/filepath"
 )
 
 type Config struct {
@@ -11,11 +12,19 @@ type Config struct {
 }
 
 func parseCommandLine() *Config {
+	// addr
 	dfltAddr := "localhost:8842"
 	addr := flag.String("addr", dfltAddr, "Address to listen on")
+
+	// credentialsFile defaults to env var, then relative path, then filename
 	dfltCredentialsFile := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
 	if dfltCredentialsFile == "" {
-		dfltCredentialsFile = "service-account.json"
+		exe, err := os.Executable()
+		if err != nil {
+			dfltCredentialsFile = "service-account.json"
+		} else {
+			dfltCredentialsFile = filepath.Join(filepath.Dir(exe), "service-account.json")
+		}
 	}
 	credentialsFile := flag.String("credentialsFile", dfltCredentialsFile, "Path to service account credentials in JSON format")
 
